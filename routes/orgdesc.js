121b99445f134fa7d68e81524d750101ddf438d6
beanLau <522405475@qq.com>
2019-11-14 23:34:16 +0800

    对接

--- routes/orgdesc.js
+++ routes/orgdesc.js
@@ -5,7 +5,6 @@ router.get('/orgdesc', async (ctx) => {
     let yfpcdata = ctx.cookies.get('yfpcdata') || '{}'
     yfpcdata = decodeURIComponent(yfpcdata)
     yfpcdata = JSON.parse(yfpcdata)
-    let zizhiList = []
     let pageContent = {
         aboutGJ: {
             info: ""
@@ -15,32 +14,19 @@ router.get('/orgdesc', async (ctx) => {
         },
         historyBack: {
             info: ""
+        },
+        zizhiZZ:{
+            info: ""
         }
-    }
-    let pageInfo = {
-        current: 1,
-        pages: 1,
-        total: 0
     }
     let urlQuery = ctx.request.query;
-    if(urlQuery.type == 2){
-        resData = await orgapi.getZizhiList(ctx,{
-            pageNo: urlQuery.page || 1,
-            pageSize: 10
-        })
-        if(resData.code == 0){
-            try {
-                zizhiList = resData.result.records || []
-                pageInfo.current = resData.result.current
-                pageInfo.pages = resData.result.pages
-                pageInfo.total = resData.result.total
-            } catch (error) {
-                
-            }
+    resData = await orgapi.getOrgInfo(ctx)
+    if(resData.code == 0){
+        try {
+            pageContent = resData.result || {}
+        } catch (error) {
+            
         }
-    }else{
-        resData = await orgapi.getOrgInfo(ctx)
-        pageContent = resData.result
     }
     
     await ctx.render('orgdesc/orgdesc', {
@@ -49,33 +35,7 @@ router.get('/orgdesc', async (ctx) => {
         yfpcdata: yfpcdata,
         urlQuery,
         pageContent: pageContent,
-        zizhiList: zizhiList,
-        orgType: urlQuery.type || 0,
-        pageInfo: pageInfo
-    })
-})
-router.get('/qualification', async (ctx) => {
-    let yfpcdata = ctx.cookies.get('yfpcdata') || '{}'
-    yfpcdata = decodeURIComponent(yfpcdata)
-    yfpcdata = JSON.parse(yfpcdata)
-    let newsDetail = {}
-    let urlQuery = ctx.request.query;
-    resNewDetail = await orgapi.getNewsDetail(ctx,{
-        id: urlQuery.id
-    });
-    if(resNewDetail.code == 0){
-        try {
-            newsDetail = resNewDetail.result || {}
-        } catch (error) {
-            
-        }
-    }
-    //type：1行业信息  2通知公告 3通用标准 4技术文献 5考核标准
-    await ctx.render('qualification/qualification', {
-        title: newsDetail.title,
-        yfpcdata: yfpcdata,
-        pagePath: ctx.request.path,
-        newsDetail: newsDetail
+        orgType: urlQuery.type || 0
     })
 })
 
