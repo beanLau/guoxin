dbdbca5154bbd605ac352d1033f9ff16df886182
liuweitao <522405475@qq.com>
2019-11-19 14:03:39 +0800

    退出登录

--- public/js/utils.js
+++ public/js/utils.js
@@ -1,5 +1,5 @@
-function getQueryString(){
-    
+function getQueryString() {
+
 }
 function setCookie(name, value, day) {
     if (day !== 0) {     //当设置的时间等于0时，不设置expires属性，cookie在浏览器关闭后删除
@@ -10,6 +10,10 @@ function setCookie(name, value, day) {
         document.cookie = name + "=" + escape(value);
     }
 };
-function delCookie (name){
+function delCookie(name) {
     setCookie(name, ' ', -1);
+}
+function getCookie(name) {
+    var arr, reg = new RegExp("(^| )" + name + "=([^;]*)(;|$)");
+    return (arr = document.cookie.match(reg)) ? unescape(arr[2]) : null;
 }
